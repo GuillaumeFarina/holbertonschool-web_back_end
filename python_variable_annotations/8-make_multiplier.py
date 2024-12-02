42c91812f794fe3672dc497eb47a74906bd4a38f
Wqkz <guillaume.farina@hotmail.com>
2024-12-02 14:03:14 +0100

    Fix pycodestyle, delete a blank line

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Takes a float multiplier as argument and 
+""" Takes a float multiplier as argument and
     returns a function that multiplies a float by multiplier
 """
 from typing import Callable

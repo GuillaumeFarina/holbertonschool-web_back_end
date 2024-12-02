559e7e54d85f41c9a68185a236b8e33efb723d54
Wqkz <guillaume.farina@hotmail.com>
2024-12-02 15:28:32 +0100

    Add description for pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Spawns task_wait_random n times with the specified max_delay. """
 import asyncio
 from typing import List
 
@@ -7,7 +8,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns task_wait_random n times with the specified max_delay.
     Returns the list of all the delays in ascending order.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
